2d231902887d6b53c8db7a5ca2502d4c687fb3fa
Paritosh Jha <paritoshjha4@gmail.com>
2021-05-31 10:38:02 -0700

    introduction: alert, live server extension, comments, connecting JS file, short intro to variables

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log("WAZZAAAAA");
+console.error("errooororr");
+console.warn("warrrning");
+
+let color = "red";
+color = "blue";
+
+
+/* use console for debugging statements
+    - can run JS straight through console
+    - Or use console.log in code
+    - use https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
+    for any documentation needs
+*/
+
+
+/*
+Three variables: var, let const
+
+var is globally scoped
+let and const have block level scope
+
+let allows you to re-assign values (see line 5,6)
+only use const for values that are unchanging
+
+
+
+
+
+*/
+
+
